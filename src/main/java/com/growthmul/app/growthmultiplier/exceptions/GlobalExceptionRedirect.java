178cd3dc61b0328bb00dfe787abeef773d558fa3
Commander-Ayush <ayushkrjha221004@gmail.com>
2026-07-30 16:40:31 +0530

    initialized global exception

---
+++ src/main/java/com/growthmul/app/growthmultiplier/exceptions/GlobalExceptionRedirect.java
@@ -0,0 +1,7 @@
+package com.growthmul.app.growthmultiplier.exceptions;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionRedirect {
+}
